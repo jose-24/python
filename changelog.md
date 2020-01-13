b53533acf7bf2fc9d70bc33d4836b6cc30b01523
jose <jose-4n@hotmail.com>
2020-01-13 17:55:58 -0500

    1.0.0

---
+++ changelog.md
@@ -0,0 +1,6 @@
+#python course
+
+##version 1.0.0
+there is a "hello world" script in python
+
+    A script that
---
+++ newvar.py
@@ -0,0 +1,45 @@
+#un comentario salvaje en python
+
+
+#x=10;
+#y=10.0;
+#phrase='Hello world!'
+#print(x);
+#print(y);
+#print(phrase);
+#print(type(x));  
+#print(type(y));  
+#print(type(phrase))
+#x=6/5#division flotante
+#y=6//5#division entera
+#z=6*5#multiplicacion
+#a=2**3#potencia
+#print(x)
+#print(y)
+#print(z)
+#print(a)
+
+#recibir parametro por teclado
+
+variable = input('ingrese un entero: ')
+print(variable)
+print(type(variable))#de esta manera bota un string
+
+variable = int(input('ingrese un entero: '))
+print(variable)
+print(type(variable))#de esta manera se castea a int(se vuelve int)
+
+#Hallar el radio de una circunferencia
+variable = int(input('INGRESE EL RADIO DEL CIRCULO: '))#es una variable int
+variable=variable**2
+variable=3.14*variable#se vuelve una variable flotante
+variable=int(variable)#se vuelve un entero
+print('EL RADIO DEL CIRCULO ES:',variable)
+print(type(variable))
+
+#Hallar el radio de una circunferencia
+    math
+radio=float(input('ingrese el radio'))
+area=round(math.pi*radio**2,2)
+print(f'el area'(area))
+
